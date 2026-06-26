06da63e71494dbac3f25cc25204e7c537d0627d1
sh0723 <pshpark12345@naver.com>
2026-06-26 12:58:14 +0900

    15684 : 사다리 조작 solve

---
+++ src/week3/BOJ15684/BOJ15684_V1.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N, H, M;
+int visited[31][11];
+int ret = INT_MAX;
+bool check() {
+    for (int i=1; i<N; i++) {
+        int start = i;
+        for (int j=1; j<=H; j++) {
+            if (visited[j][start]) start++;
+            else if (visited[j][start-1]) start--;
+        }
+        if (start != i)
+            return false;
+    }
+    return true;
+}
+
+void solve(int here, int cnt) {
+    if (cnt > 3 || cnt >= ret)
+        return;
+
+    if (check()) {
+        ret = min(ret, cnt);
+        return;
+    }
+
+    for (int i=here; i<=H; i++) {
+        for (int j=1; j<N; j++) {
+            if (visited[i][j] || visited[i][j-1] || visited[i][j+1]) continue;
+            visited[i][j] = 1;
+            solve(i, cnt + 1);
+            visited[i][j] = 0;
+        }
+    }
+}
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M >> H;
+
+    for (int i=0; i<M; i++) {
+        int y,x;
+        cin >> y >> x;
+        visited[y][x] = 1;
+    }
+    solve(1, 0);
+    if (ret == INT_MAX)
+        cout << -1 << '\n';
+    else
+        cout << ret << '\n';
+    return 0;
+}
