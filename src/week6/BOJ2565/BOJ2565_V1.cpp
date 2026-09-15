7daddc281ac1b8d902443307a4b9f381fc51b3bf
sh0723 <pshpark12345@naver.com>
2026-09-15 15:51:44 +0900

    2565 : 전깃줄 solve

---
+++ src/week6/BOJ2565/BOJ2565_V1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> lis;
+vector<pair<int, int>> inp_arr;
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N;
+    cin >> N;
+    for (int i=0; i<N; i++) {
+        int a,b;
+        cin >> a >> b;
+        inp_arr.push_back({a,b});
+    }
+
+    sort(inp_arr.begin(), inp_arr.end());
+
+    for (int i=0; i<N; i++) {
+        int num = inp_arr[i].second;
+
+        auto _pos = lower_bound(lis.begin(), lis.end(), num);
+
+        if (_pos == lis.end()) {
+            lis.push_back(num);
+        } else {
+            *_pos = num;
+        }
+    }
+
+    cout << N-lis.size();
+
+    return 0;
+}
