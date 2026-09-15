d5417e79ebb59501a740b92c246600b0998cb355
sh0723 <pshpark12345@naver.com>
2026-09-15 15:29:01 +0900

    14002 : 가장 긴 증가하는 부분 수열 4 solve

---
+++ src/week6/BOJ14002/BOJ14002_V1.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[1000], cnt[1000], ret=1, idx;
+int _prev[1000];
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    fill(cnt, cnt+1000, 1);
+    fill(_prev, _prev+1000, -1);
+    int N;
+    cin >> N;
+
+    for (int i=0; i<N; i++) {
+        cin >> arr[i];
+    }
+
+
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<i; j++) {
+            if (arr[i] > arr[j] && cnt[j] + 1 > cnt[i]) {
+                cnt[i] = cnt[j] + 1;
+                _prev[i] = j;
+                if (ret < cnt[i]) {
+                    ret = cnt[i];
+                    idx = i;
+                }
+            }
+        }
+    }
+    cout << ret << '\n';
+
+    stack<int> stk;
+    while(true) {
+        if (idx == -1) break;
+        stk.push(arr[idx]);
+        idx = _prev[idx];
+    }
+
+    while(stk.size()) {
+        cout << stk.top() << ' ';
+        stk.pop();
+    }
+
+
+    return 0;
+}
